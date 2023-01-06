ba4a8a78d3ce8fc88c51a33f86460c7c261a5a7b
vyacheslav833 <zv.specialone@gmail.com>
2023-01-06 14:43:41 +0300

    Подключение библиотеки AssertJ [#504881 #378181] - corrected

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ArrayItTest {
+
     @Test
     void whenMultiCallHasNextThenTrue() {
         ArrayIt it = new ArrayIt(
