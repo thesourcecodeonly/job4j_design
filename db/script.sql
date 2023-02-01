734b23f6bd00de09908783346b54cab9ec3d1479
vyacheslav833 <zv.specialone@gmail.com>
2023-02-01 10:58:31 +0300

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,11 @@
+create table students(id serial primary key, firstname varchar(255), lastname varchar(255),
+	facultyname varchar(255), facultybudget bool,
+	age int
+);
+
+insert into students(firstname, lastname, facultyname, facultybudget, age)
+    values('Ivan', 'Petrov', 'Mathematics', true, 22);
+
+update students set firstname = 'John';
+
+delete from students where firstname = 'John';
